1c1b88c6e669734125209935cc1e4227194d6cfa
Boubacar Camara <boubacarbakarycamara@esp.sn>
2026-01-26 17:41:20 +0000

    Second serveur Express sur port 4000

--- small-server/app.js
+++ small-server/app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const app = express()
+const app = express();
 const port = 3000
 
 app.get('/index', (req, res) => {
@@ -8,6 +8,22 @@ app.get('/index', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+
+// serveur 2 :
+
+const app2 = express();
+
+const port2 = 4000;
+
+app2.get('/apropos', (req, res) => {
+  res.send('Deuxieme serveur avec port 4000');
+});
+
+
+app2.listen(port2, () => {
+  console.log(`Serveur 2 lancé sur le port : ${port2}`);
 })
 
 /// app.method(path, handler)
